e57e2979cc8a2bff207644b18c3659d41ac3cfac
Keshav Sharma <sharma.keshav39@gmail.com>
2021-06-13 16:07:50 +0530

    Add Pangram Project Files (#5)

---
+++ csharp/Exercism-CSharp/pangram/Pangram.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class Pangram
+{
+    const string alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+    public static bool IsPangram(string input)
+    {
+        return alphabet.All(input.ToLower().Contains);
+    }
+}
